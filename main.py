4943dd950084c155770dffbc08831ffbc44e2fc3
arnasarn <sirvydasarnas@gmail.com>
2026-04-24 12:23:05 +0300

    refactoring, throttle function

--- main.py
+++ main.py
@@ -3,35 +3,55 @@ import tkinter as tk
 #Pagrindinė aplikacijos klasė
 class App:
     def __init__(self, root):
-        self.engine = Engine()
+        self._engine = Engine(ECU())
+        self._rpm_label = tk.Label(root, text=f"RPM: {self._engine._rpm}", font=("Arial", 16))
+        self._rpm_label.pack(pady=20)
 
-        self.label = tk.Label(root, text=f"RPM: {self.engine.rpm}", font=("Arial", 16))
-        self.label.pack(pady=20)
+        self._throttle_label = tk.Label(root, text=f"Throttle: {self._engine._throttle}", font=("Arial", 16))
+        self._throttle_label.pack(pady=20)
 
-        root.bind("<Up>", self.increase)
-        root.bind("<Down>", self.decrease)
+
+        root.bind("<KeyPress-Up>", self.throttle_on)
+        root.bind("<KeyRelease-Up>", self.throttle_off)
 
     def update_display(self):
-        self.label.config(text=f"RPM: {self.engine.rpm}")
+        self._rpm_label.config(text=f"RPM: {self._engine._rpm}")
+        self._throttle_label.config(text=f"Throttle: {self._engine._throttle}")
 
     def increase(self, event):
         self.engine.increase_rpm()
         self.update_display()
 
-    def decrease(self, event):
-        self.engine.decrease_rpm()
+    def throttle_on(self, event):
+        self._engine.set_throttle(1)
         self.update_display()
+
+    def throttle_off(self, event):
+        self._engine.set_throttle(0)
+        self.update_display()
+
 
 #Variklio klasė
 class Engine:
-    def __init__(self):
-        self.rpm = 800
+    def __init__(self, ecu):
+        # Konfigūracija
+        self._max_rpm = 7000
+        self._idle_rpm = 800
+        self._max_temp = 120
+        self._ecu = ecu
+        
+        # Būsena
+        self._rpm = self._idle_rpm
+        self._temperature = 70
+        self._throttle = 0
+        self._fuel_used = 0.0
 
-    def increase_rpm(self):
-        self.rpm += 100
+    def set_throttle(self, value):
+        if (value != 0 and value != 1):
+            raise ValueError("Throttle can only be numbers 0 or 1")
 
-    def decrease_rpm(self):
-        self.rpm = max(0, self.rpm - 100)
+        self._throttle = value
+
 
 #Tėvinė jutiklio klasė ir klasės, kurios ją paveldi
 class Sensor:
@@ -42,6 +62,7 @@ class Sensor:
 class TemperatureSensor(Sensor):
     def read(self):
         return 90  # example value
+
 
 #Įrenginių tėvinė klasė ir klasės, kurios ją paveldi
 class Device:
@@ -66,9 +87,12 @@ class ECU:
         else:
             self.fan.activate("LOW")
 
+
 #Programos inicializavimas
 root = tk.Tk()
 root.title("ECU Simulator")
 root.geometry("400x300")
+ecu = ECU()
+engine = Engine(ecu)
 app = App(root)
 root.mainloop()
